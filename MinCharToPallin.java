8a408518713a6a7537c16e4be4d4f6cac8e40183
Shri_shinigami <35933110+shrishriyesh@users.noreply.github.com>
2020-06-17 19:53:00 +0530

    revisit
    https://www.interviewbit.com/problems/minimum-characters-required-to-make-a-string-palindromic/

---
+++ MinCharToPallin.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int solve(String A) {
+        int n = A.length();
+        int ans = n;
+        while(n>1 && !isPalindrome(A, n)) {
+            n--;
+        }
+        return ans-n;
+    }
+    public boolean isPalindrome(String A, int len) {
+        int i=0, j=len-1;
+        while(i<=j && (A.charAt(i) == A.charAt(j))) {
+            i++;j--;
+        }
+        if(i>j) {
+            return true;
+        }
+        return false;
+    }
+}
