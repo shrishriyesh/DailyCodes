dc3abe0182a882b9b0db09152cb4afbf768fc098
Shri_shinigami <shriyesh.chandra@gmail.com>
2020-08-03 21:55:54 +0530

    DP-2 (Faaltu)
    https://www.interviewbit.com/problems/best-time-to-buy-and-sell-stocks-i/

---
+++ BuyAndSellStocks1.java
@@ -0,0 +1,21 @@
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public int maxProfit(final List<Integer> A) {
+        if(A.size()==0)
+        return 0;
+        int min=A.get(0);
+        int max=0;
+        for(int i=0;i<A.size();i++)
+        {
+            if(A.get(i)<=min)
+            {
+                min=A.get(i);
+            }
+            else
+            {
+                max=Math.max(max,A.get(i)-min);
+            }
+        }
+        return max;
+    }
+}
